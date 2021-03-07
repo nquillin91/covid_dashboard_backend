9d25afa45468eecbd4d1a64fb511a391037086bb
Nicholas Quillin <nquillin91@gmail.com>
2021-03-07 16:17:20 -0600

    Updating VaccineDataService to have the service annotation - originally missed on first pass of updates

--- src/main/java/com/covid/dashboard/service/VaccineDataService.java
+++ src/main/java/com/covid/dashboard/service/VaccineDataService.java
@@ -1,7 +1,10 @@
 package com.covid.dashboard.service;
 
+import org.springframework.stereotype.Service;
+
 import com.covid.dashboard.dto.VaccineDataDTO;
 
+@Service
 public class VaccineDataService {
 	
 	public VaccineDataDTO getVaccineData() {
